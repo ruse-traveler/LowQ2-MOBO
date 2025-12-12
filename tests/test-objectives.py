9f4d2506e137e225d8761fc4de31d61d1e0d6db0
ruse-traveler <derek.murphy.anderson@protonmail.com>
2025-12-12 11:38:27 -0500

    Update objective test for LowQ2

--- tests/test-objectives.py
+++ tests/test-objectives.py
@@ -7,22 +7,35 @@
 #    methods
 #
 #  TODO convert to use pytest
-#  TODO update to be better for Low-Q2
 # =============================================================================
 
 import sys
 sys.path.append('../')
 
-import objectives.LowQ2RecoResolution as eres
+import objectives.LowQ2GlobalResolution as lqg
 
-# test resolution calculation on electrons
-ele_reso = eres.CalculateReso(
-    ifile = "root://dtn-eic.jlab.org//volatile/eic/EPIC/RECO/25.07.0/epic_craterlake/SINGLE/e-/5GeV/45to135deg/e-_5GeV_45to135deg.0099.eicrecon.edm4eic.root",
-    ofile = "test_reso.ele.root",
-    pdg   = 11
+# test 0: run objectives ------------------------------------------------------
+
+# output file names for convenience
+ofGloRes = "test_globa_reso.root"
+
+# test global resolution
+glo_reso = lqg.CalcaulateMomReso(
+    "root://dtn-eic.jlab.org//volatile/eic/EPIC/RECO/25.07.0/epic_craterlake/SINGLE/e-/5GeV/45to135deg/e-_5GeV_45to135deg.0099.eicrecon.edm4eic.root",
+    ofGloRes
 )
 
-print(f"Ran objectives:")
-print(f"  -- e- resolution = {ele_reso}")
+print(f"[0] Ran objectives:")
+print(f"  -- global p resolution = {glo_reso}")
+
+# test 1: extract objectives --------------------------------------------------
+
+# extract global resolution
+glo_reso_txt = None
+with open(ofGloRes.replace(".root", ".txt")) as oglo:
+    glo_reso_txt = float(oglo.read().splitlines()[0])
+
+print(f"[1] Extracted objectives:")
+print(f"  -- global p resolution = {glo_reso_test}, type = {type(glo_reso_txt)}")
 
 # end =========================================================================
