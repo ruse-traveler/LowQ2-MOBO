707a94c160cc8ab82ec5585abd4f5ddd588a434c
ruse-traveler <derek.murphy.anderson@protonmail.com>
2025-12-12 17:48:47 -0500

    Fix typos and rely on macro files being set by SimGenerator

--- steering/electron/backward.e18ele.py
+++ steering/electron/backward.e18ele.py
@@ -2,10 +2,8 @@ from DDSim.DD4hepSimulation import DD4hepSimulation
 from g4units import m
 
 SIM = DD4hepSimulation()
-SIM.enableG4GPS
 
 SIM.runType          = "run"
-SIM.macroFile        = "./backward.e18ele.py"
-SIM.physics.rangeCut = 100.0*m
+SIM.physics.rangecut = 100.0*m
 
 # NOTE num events set in macro file
